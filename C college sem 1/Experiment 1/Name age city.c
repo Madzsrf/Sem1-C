202f44b1ed1d74fb4b85dbacc2543984d0289679
Madhav Sarraf <creator.madhav@gmail.com>
2025-09-01 14:49:02 +0530

    Done 5!

--- C college sem 1/Experiment 1/Name age city.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    printf("Name: Madhav Sarraf\nAge:18\nCity:Birgunj");
-    return 0;
-}
--- C college sem 1/Experiment 1/address.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("P-166, Block B\nLaketown, South DumDum,\nKolkata,700089");
-    return 0;
-}
